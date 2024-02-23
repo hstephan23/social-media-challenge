16da1dad3380a93d40a509528b2b95a818c47d1a
hstephan23 <harrison@stephanclan.com>
2024-02-22 22:16:34 -0700

    added the get routes for thoughts

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,4 +1,24 @@
 const router = require('express').Router();
 const { Thought } = require('../../models');
 
+// route to get all thoughts
+router.get('/', async (req, res) => {
+    try {
+        const thoughtsData = await Thought.find();
+        res.status(200).json(thoughtsData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// route to get a single thought by id
+router.get('/:thoughtId', async (req, res) => {
+    try {
+        const thoughtData = await Thought.findOne({ _id: req.params.thoughtId });
+        res.status(200).json(thoughtData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
 module.exports = router;
